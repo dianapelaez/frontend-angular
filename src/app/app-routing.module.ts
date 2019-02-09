0cd4953c01acf8bdad5daeb3d753fe7d8e22a50e
dianapelaez <peldiana@gmail.com>
2019-02-08 22:58:02 -0500

    Titulares perosnas fisicas list created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { AppComponent } from './app.component';
+import { PersonaFisicaComponent } from './persona-fisica/persona-fisica.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/userComponent', pathMatch: 'full'},
-  {path: 'appComponent', component: AppComponent}
+  {path: '', redirectTo: '/personaFisicaComponent', pathMatch: 'full'},
+  {path: 'appComponent', component: AppComponent},
+  {path: 'personaFisicaComponent', component: PersonaFisicaComponent}
  ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
